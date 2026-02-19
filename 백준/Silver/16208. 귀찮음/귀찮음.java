2cbcd2ce27cfc1445d1495639d94150565faeb5a
dlgudwn55 <dkrlekeheoek@naver.com>
2026-02-19 14:47:19 +0900

    [Silver V] Title: 귀찮음, Time: 100 ms, Memory: 14324 KB, Score: 4 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] a = new int[n];
+        String line = br.readLine();
+        String[] tokens = line.split("\\s+");
+        
+        for (int i = 0; i < n; i++)
+    	a[i] = Integer.parseInt(tokens[i]);
+        
+        Arrays.sort(a);
+        
+        int[] right = new int[n];
+        for (int i = n-1; i >= 0; i--) {
+    	if (i == n-1)
+    	    right[i] = a[i];
+    	else
+    	    right[i] = a[i] + right[i+1];  
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < n-1; i++)
+    	ans += a[i] * right[i+1];
+        System.out.println(ans);
+    }
+}
