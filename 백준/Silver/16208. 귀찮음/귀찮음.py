0deb1fbfb3ab6f0c6f907f0e95ae42e444df7af5
dlgudwn55 <dkrlekeheoek@naver.com>
2026-02-19 14:37:23 +0900

    [Silver V] Title: 귀찮음, Time: 228 ms, Memory: 181488 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.py
+++ 백준/Silver/16208. 귀찮음/귀찮음.py
@@ -3,9 +3,12 @@ a = list(map(int, input().split()))
 
 a.sort()
 ans = 0
+right = [a[i] for i in range(n)]
+for i in range(n-2, -1, -1):
+    right[i] += right[i+1]
 
-for i in range(n):
-    cost = a[i] * sum(a[i+1:])
+for i in range(n-1):
+    cost = a[i] * right[i+1]
     ans += cost
 
 print(ans)
