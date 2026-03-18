b099c7cabeb98514dcc21926834b59ccb9df49f7
dlgudwn55 <dkrlekeheoek@naver.com>
2026-03-18 15:24:32 +0900

    [Bronze V] Title: 공통교육과정, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,11 @@
+lecture_code = input()
+section = lecture_code[0]
+
+if section == 'F':
+    print('Foundation')
+elif section == 'C':
+    print('Claves')
+elif section == 'V':
+    print('Veritas')
+elif section == 'E':
+    print('Exploration')
