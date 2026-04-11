a2dae2c4819a6b97cc6416b84db3d3b8f6a48663
dlgudwn55 <dkrlekeheoek@naver.com>
2026-04-11 15:26:15 +0900

    [Silver III] Title: Eeny Meeny Moo, Time: 332 ms, Memory: 114640 KB -BaekjoonHub

---
+++ 백준/Silver/6602. Eeny Meeny Moo/Eeny Meeny Moo.py
@@ -0,0 +1,25 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    m = 0
+    flag = False
+    for i in range(1, 1000):
+        q = deque([i for i in range(n)])
+        while True:
+            rm = q.popleft()
+            if rm == 1 and not q:
+                m = i
+                flag = True
+                break
+            elif rm == 1 and q:
+                break
+            for _ in range(i):
+                q.append(q.popleft())
+        if flag:
+            break
+    print(m + 1)
