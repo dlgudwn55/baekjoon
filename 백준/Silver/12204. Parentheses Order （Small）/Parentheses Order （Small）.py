2ae3208f96b04f1ef7700c3da66c15389f56c8ce
dlgudwn55 <dkrlekeheoek@naver.com>
2026-04-15 10:02:33 +0900

    [Silver III] Title: Parentheses Order (Small), Time: 460 ms, Memory: 132876 KB -BaekjoonHub

--- 백준/Silver/12204. Parentheses Order （Small）/Parentheses Order （Small）.py
+++ 백준/Silver/12204. Parentheses Order （Small）/Parentheses Order （Small）.py
@@ -12,19 +12,23 @@ def check(seq: list):
             st.pop()
     return len(st) == 0
 
-def generate_par_seq(seq: list, mx_len):
+def generate_par_seq(seq: list, mx_len, open_par_count):
     if len(seq) == mx_len:
-        if check(seq):
+        # if check(seq):
+        if open_par_count == 0:
             par_seq_lst.append(''.join(seq))
         return
-    for par in "()":
-        generate_par_seq(seq + [par], mx_len)
+    # for par in "()":
+    generate_par_seq(seq + ['('], mx_len, open_par_count + 1)
+
+    if open_par_count > 0:
+        generate_par_seq(seq + [')'], mx_len, open_par_count - 1)
 
 t = int(input())
 for tn in range(1, t+1):
     n, k = map(int, input().split())
     par_seq_lst = []
-    generate_par_seq([], n*2)
+    generate_par_seq([], n*2, 0)
     
     print(f"Case #{tn}: ", end="")
     if k > len(par_seq_lst):
